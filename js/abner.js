b3c53ec4a781e26dd5b42d9989efbd96825519e9
abner211198 <82497985+abner211198@users.noreply.github.com>
2022-07-23 03:03:39 -0500

    Update abner.js

--- js/abner.js
+++ js/abner.js
@@ -1,12 +1,12 @@
-// var nav = document.querySelector("nav");
+var nav = document.querySelector("nav");
 
-// window.addEventListener("scroll", function () {
-//   if (window.pageYOffset > 100) {
-//     nav.classList.add("bg-dark", "navbar-dark");
-//   } else {
-//     nav.classList.remove("bg-dark", "navbar-light");
-//   }
-// });
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 100) {
+    nav.classList.add("bg-dark", "navbar-dark");
+  } else {
+    nav.classList.remove("bg-dark", "navbar-light");
+  }
+});
 document.addEventListener("click", function (e) {
   if (e.target.classList.contains("gallery-item")) {
     const src = e.target.getAttribute("src");
